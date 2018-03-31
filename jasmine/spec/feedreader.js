5e66cb9c5da2fc406280ec2e8bcbac894a31477b
Irene <gummyrene@gmail.com>
2018-03-31 16:59:35 -0700

    removed ) from line 77

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -74,7 +74,7 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
         beforeEach(function(done) {
-            loadFeed(0, done() {
+            loadFeed(0, done) {
          });
         
          it('are present', function() {
